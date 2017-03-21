e5237ab3f4bd261c80921db254fca56d7617a63b
taewan0530 <kimtaewan@kimtaewan-ui-Mac-mini.local>
2017-03-21 14:59:50 +0900

    Initial commit

---
+++ Sources/RxBindNext.swift
@@ -0,0 +1,24 @@
+//
+//  RxBindFunc.swift
+//  RxBindFunc
+//
+//  Created by kimtaewan on 2017. 3. 16..
+//  Copyright © 2017년 taewan. All rights reserved.
+//
+//http://blog.xebia.com/function-references-in-swift-and-retain-cycles/
+//https://medium.com/@gnod94/improvements-of-flatmap-function-in-rxswift-5d70add0fc88#.mjmtg1sp3
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+public extension ObservableType {
+    
+    public func bindNext<A: AnyObject>(weak obj: A, _ onNext: @escaping (A) -> ((Self.E) -> Swift.Void)) -> Disposable {
+        return self.bindNext { [weak obj] (value: Self.E) in
+            guard let `self` = obj else { return }
+            onNext(self)(value)
+        }
+    }
+}
+

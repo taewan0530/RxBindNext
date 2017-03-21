f4196c60d669cb77f2f9d477d20b99385b260eb2
taewan0530 <taewan0530@daum.net>
2017-03-21 23:35:48 +0900

    Carthage 추가 선행 작업

---
+++ RxBindNext-iOS/RxBindNext-iOS.h
@@ -0,0 +1,19 @@
+//
+//  RxBindNext-iOS.h
+//  RxBindNext-iOS
+//
+//  Created by taewan on 2017. 3. 21..
+//  Copyright © 2017년 taewan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxBindNext-iOS.
+FOUNDATION_EXPORT double RxBindNext_iOSVersionNumber;
+
+//! Project version string for RxBindNext-iOS.
+FOUNDATION_EXPORT const unsigned char RxBindNext_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxBindNext_iOS/PublicHeader.h>
+
+
--- Sources/RxBindNext.swift
+++ Sources/RxBindNext.swift
@@ -1,10 +1,25 @@
+// The MIT License (MIT)
 //
-//  RxBindFunc.swift
-//  RxBindFunc
+// Copyright (c) 2017 taewan kim
 //
-//  Created by kimtaewan on 2017. 3. 16..
-//  Copyright © 2017년 taewan. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 //http://blog.xebia.com/function-references-in-swift-and-retain-cycles/
 //https://medium.com/@gnod94/improvements-of-flatmap-function-in-rxswift-5d70add0fc88#.mjmtg1sp3
 
@@ -13,7 +28,7 @@ import RxSwift
 import RxCocoa
 
 public extension ObservableType {
-    
+
     public func bindNext<A: AnyObject>(weak obj: A, _ onNext: @escaping (A) -> ((Self.E) -> Swift.Void)) -> Disposable {
         return self.bindNext { [weak obj] (value: Self.E) in
             guard let `self` = obj else { return }
